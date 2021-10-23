b5926d02e10f97b16e4f2c44b5696d2cdbbb9437
CHEN TONG <zhentong98@hotmail.com>
2021-10-23 06:54:38 +0000

    - update

--- database.py
+++ database.py
@@ -2,11 +2,21 @@ import mysql.connector
 
 
 def initDatabaseConnection():
+    # Localhost
+    # dbConn = mysql.connector.connect(
+    #     host="localhost",
+    #     user="root",
+    #     password="",
+    #     database='ecommerce',
+    #     port=3307
+    # )
+    
+    # Server
     dbConn = mysql.connector.connect(
         host="localhost",
-        user="root",
-        password="",
+        user="zhentong",
+        password="yjp!fgv.kzf9BPA2dat",
         database='ecommerce',
-        port=3307
+        port=3306
     )
     return dbConn
